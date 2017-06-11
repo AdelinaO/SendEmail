b61a08ef58c4a29efcfcf587f55ad7a3c32f796a
AdelinaO <oprea.adelina91@gmail.com>
2017-06-11 18:37:18 +0300

    project 1st commit

---
+++ SendAnEmail/Specification/SpecFlowFeature1.feature
@@ -0,0 +1,11 @@
+﻿Feature: Gmail login
+
+Scenario: Login to the gmail
+
+  Given I navigated to gmail page
+  When  I choose 'Use another account' option (#identifierLink > div.vdE7Oc.f3GIQ) (//*[@id="identifierLink"]/div[2] xpath)
+  And   I type the email (#identifierId)(//*[@id="identifierId"] xpath)
+  And   I execute the next button (#identifierNext > content > span)(//*[@id="identifierNext"]/content xpath)
+  And   I type the password (#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input)(//*[@id="password"]/div[1]/div/div[1]/input xpath)
+  And   I execute the next button (#passwordNext > content)(//*[@id="passwordNext"]/content xpath)
+  Then  I should be logged in --assert
---
+++ SendAnEmail/StepDefinition/GmailLoginSteps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+
+
+
+namespace SendAnEmail
+
+{
+    [Binding]
+    public class GmailLoginSteps
+    {
+        public object I { get; private set; }
+
+        [Given(@"I navigated to gmail page")]
+        public void GivenINavigatedToGmailPage()
+        {
+
+        }
+        
+        [When(@"I choose '(.*)' option \(\#identifierLink > div\.vdE(.*)Oc\.f(.*)GIQ\) \(//\*\[@id=""(.*)""]/div\[(.*)] xpath\)")]
+        public void WhenIChooseOptionIdentifierLinkDiv_VdEOc_FGIQIdDivXpath(string p0, int p1, int p2, string p3, int p4)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I type the email \(\#identifierId\)\(//\*\[@id=""(.*)""] xpath\)")]
+        public void WhenITypeTheEmailIdentifierIdIdXpath(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I execute the next button \(\#identifierNext > content > span\)\(//\*\[@id=""(.*)""]/content xpath\)")]
+        public void WhenIExecuteTheNextButtonIdentifierNextContentSpanIdContentXpath(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I type the password \(\#password > div\.aCsJod\.oJeWuf > div > div\.Xb(.*)hP > input\)\(//\*\[@id=""(.*)""]/div\[(.*)]/div/div\[(.*)]/input xpath\)")]
+        public void WhenITypeThePasswordPasswordDiv_ACsJod_OJeWufDivDiv_XbhPInputIdDivDivDivInputXpath(int p0, string p1, int p2, int p3)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I execute the next button \(\#passwordNext > content\)\(//\*\[@id=""(.*)""]/content xpath\)")]
+        public void WhenIExecuteTheNextButtonPasswordNextContentIdContentXpath(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should be logged in --assert")]
+        public void ThenIShouldBeLoggedIn_Assert()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
